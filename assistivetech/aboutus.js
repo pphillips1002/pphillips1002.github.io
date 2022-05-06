4710f7926dbcef8242ae8357806e284a9eae33a5
Pierce Phillips <55248632+pphillips1002@users.noreply.github.com>
2022-05-06 09:32:41 -0400

    in-process edits

--- assistivetech/aboutus.js
+++ assistivetech/aboutus.js
@@ -4,3 +4,14 @@ document.getElementById("home-id").addEventListener("click", returnHome);
 function returnHome() {
     window.location.href = "index.html";
 }
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
